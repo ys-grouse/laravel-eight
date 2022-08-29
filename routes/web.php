fc23ee63933930ca7c266d3068bacb1564143902
ys-grouse <malsawma7777@gmail.com>
2022-08-30 01:21:46 +0530

    add get users

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/users', function () {
+    return DB::select('select * from users');
+    // return
 });
